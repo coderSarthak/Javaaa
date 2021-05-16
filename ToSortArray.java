7c65a2ad326420ad7cb1fe88599c2a8a5b6a519f
coderSarthak <68495738+coderSarthak@users.noreply.github.com>
2021-05-16 17:18:23 +0530

    Add files via upload

---
+++ ToSortArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class ToSortArray
+{
+ public static void main(String[] args)
+  {
+    int[] array={1,4,6,-2,2,5};
+    Arrays.sort(array);
+    System.out.println(Arrays.toString(array));
+   /* for(int i:array)
+    {
+      System.out.print(" "+i);
+    }
+   */
+  }
+}
